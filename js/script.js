344152038c214dd1a154ef6f30fa3e97fade5db5
Seyyed Mohammad Hosein Salim Bahrami <59095451+ARGO1379@users.noreply.github.com>
2022-08-13 11:34:29 +0430

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+﻿let $ = document
+let menu = $.querySelector('.context-menu')
+
+window.addEventListener('contextmenu',function (event) {
+    event.preventDefault()
+    console.log(event);
+    menu.style.display= 'block'
+    menu.style.top = event.pageY + 'px'
+    menu.style.left = event.pageX + 'px'
+})
+window.addEventListener('click' ,function(){
+    menu.style.display= 'none'
+})
+window.addEventListener('keydown' ,function(event){
+    if (event.which === 27) {
+        menu.style.display= 'none'
+    }
+})
